fa3200dd15e3634acd4033567de93e3f3b298db4
cheng <807992213@qq.com>
2020-03-12 15:16:22 +0800

    config

--- configserver/src/main/java/com/sdstc/config/ConfigServerStart.java
+++ configserver/src/main/java/com/sdstc/config/ConfigServerStart.java
@@ -2,21 +2,14 @@ package com.sdstc.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
-public class ConfigServerStart extends SpringBootServletInitializer {
+public class ConfigServerStart {
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerStart.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ConfigServerStart.class);
-	}
+        SpringApplication.run(ConfigServerStart.class, args);
+    }
 }
